65a77d7ab707edb4d469e3676f6031a4f1ed47f7
ℚ <mcaustin1@gmail.com>
2018-12-05 12:25:41 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+		<router-view />
+	</div>
+</template>
+
+<style lang="scss">
+	#app {
+		margin: 0;
+		background-color: #141414;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	html {
+		background: url(assets/bg.png) no-repeat center center fixed;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+	}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="home">
+		<Main />
+	</div>
+</template>
+
+<style lang="scss">
+	* {
+		background-color: inherit;
+	}
+
+</style>
+
+<script>
+	// @ is an alias to /src
+	import Main from '@/components/Main.vue'
+
+	export default {
+		name: 'home',
+		components: {
+			Main
+		}
+	}
+
+</script>
