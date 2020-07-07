6bcb3811c19fa25d73bede4fd8327dfc0f25757a
Austin McCalley <austin@austinmccalley.com>
2020-07-07 16:27:10 -0700

    New look

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,86 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
-import Date from '../components/date'
-import { getSortedPostsData } from '../lib/posts'
 
 
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hardworking and motivated computer science student with proven leadership and organizational skills. Passionate about development, coding with modern languages</p>
+        <p>Hardworking and motivated computer science student with proven leadership and organizational skills. Passionate about development, coding with modern languages.</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Projects & Experience</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
+          <li className={utilStyles.listItem}>
+            <Link href="https://www.npmjs.com/package/stockfinder">
+              <a target="_blank">StockFinder</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              A light weight query tool for the IEX Cloud stock database. <br />
+              Key Features
+              <ui className={utilStyles.smallList}>
+                <li className={utilStyles.smallListItem}>
+                  TypeScript
+                </li>
+                <li className={utilStyles.smallListItem}>
+                  Node Fetch
+                </li>
+              </ui>
+            </small>
+          </li>
+          
+          <li className={utilStyles.listItem}>
+            <Link href="https://discord.gg/devcord">
+              <a target="_blank">Devcord - Admin</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <small className={utilStyles.bold}>Sept. 2019 - Present </small>
               <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
+              A discord community that unites web developers
+            </small>
+          </li>
 
-
-
-          ))}
+          <li className={utilStyles.listItem}>
+            <Link href="https://htcolab.org/">
+              <a target="_blank">Volunteer - Diabetes Connect-A-Thon</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <small className={utilStyles.bold}>Mar. 2018 - Jun. 2018 </small>
+              <br />
+              A volunteer software engineer working with modern websites and mobile applications. In partnership with the Digital Health Collaborative.
+              <br />
+              Stack Used
+              <ul className={utilStyles.smallList}>
+                <li className={utilStyles.smallListItem}>
+                  TypeScript
+                </li>
+                <li className={utilStyles.smallListItem}>
+                  KoaJS
+                </li>
+                <li className={utilStyles.smallListItem}>
+                  MongoDB
+                </li>
+                <li className={utilStyles.smallListItem}>
+                  NativeScript
+                </li>
+                <li className={utilStyles.smallListItem}>
+                  Flutter
+                </li>
+              </ul>
+            </small>
+          </li>
         </ul>
       </section>
     </Layout>
   )
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
 }
--- posts/art-2.md
+++
@@ -1,8 +0,0 @@
----
-title: 'Art 2'
-date: '2020-05-14'
----
-
-In this art exhibit, I graphed a heat map of the United States again using my prior art as a reference. In this one, I graphed the GDP per case in each state. We often hear that the GDP of a place is how healthy the economy is and when we cross that with the amount of COVID-19 cases, we can see how healthy the economy is per state. For example, Oregon has one of the highest GDP per case, meaning that our economy is doing relatively well compared to New York, which has the lowest GDP per case.
-
-![Shelter In Place](https://raw.githubusercontent.com/austinmccalley/engr352/master/art2/photos/2020-04-20.png "GDP per Case April 20th")
