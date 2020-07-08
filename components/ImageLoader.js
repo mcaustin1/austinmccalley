f1c4c3c00e5abce31ac6b1ba2ae504e938940a91
Austin McCalley <austin@austinmccalley.com>
2020-07-07 21:47:13 -0700

    Added lazy-loading for images

---
+++ components/ImageLoader.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const _loaded = {}
+
+class ImageLoader extends React.Component {
+  state = {
+    loaded: _loaded[this.props.src]
+  }
+
+  static defaultProps = {
+    className: '',
+    loadingClassName: 'img-loading',
+    loadedClassName: 'img-loaded'
+  }
+
+  onLoad = () => {
+    _loaded[this.props.src] = true
+    this.setState(() => ({loaded: true}))
+  }
+
+  render() {
+    let { className, loadedClassName, loadingClassName, ...props } = this.props
+
+    className = `${className} ${
+      this.state.loaded ? loadedClassName : loadingClassName
+      }`
+    
+    return (
+      <img
+        src={this.props.src}
+        onClick={this.props.onClick}
+        className={className}
+        onLoad={this.onLoad}
+      />
+    )
+  }
+}
+
+export default ImageLoader
--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,11 @@
+import LazyLoad from 'react-lazy-load';
+
 import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import ImageLoader from './ImageLoader'
+
+import utilStyles from '../styles/utils.module.css'
+import styles from './layout.module.css';
 
 const name = 'Austin McCalley'
 export const siteTitle = 'Austin McCalley'
@@ -25,31 +29,33 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <img
-              src="/images/profile.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
+            <LazyLoad debounce={false}>
+              <ImageLoader
+                src="/images/profile.jpg"
+                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                alt={name}
+              />
+            </LazyLoad>
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
-            <>
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
               <Link href="/">
-                <a>
-                  <img
-                    src="/images/profile.jpg"
-                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                    alt={name}
-                  />
-                </a>
+                <a className={utilStyles.colorInherit}>{name}</a>
               </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/">
-                  <a className={utilStyles.colorInherit}>{name}</a>
-                </Link>
-              </h2>
-            </>
-          )}
+            </h2>
+          </>
+        )}
       </header>
       <main>{children}</main>
       {!home && (
@@ -60,5 +66,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  )
+  );
 }
