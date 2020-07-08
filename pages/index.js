33851bde2393e5f07c0c5edd0e832d82a6fca8b9
Austin McCalley <austin@austinmccalley.com>
2020-07-07 21:31:59 -0700

    Fixed some semantics

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,16 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hardworking and motivated computer science student with proven leadership and organizational skills. Passionate about development, coding with modern languages.</p>
+        <p>
+          Hardworking and motivated computer science student with proven
+          leadership and organizational skills. Passionate about development,
+          coding with modern languages.
+        </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Projects & Experience</h2>
         <ul className={utilStyles.list}>
-
-
           <li className={utilStyles.listItem}>
             <Link href="https://www.npmjs.com/package/stockfinder">
               <a target="_blank">StockFinder</a>
@@ -27,14 +29,10 @@ export default function Home() {
             <br />
             <small className={utilStyles.lightText}>
               A light weight query tool for the IEX Cloud stock database. <br />
-              Key Features
+              Key Features:
               <ul className={utilStyles.smallList}>
-                <li className={utilStyles.smallListItem}>
-                  TypeScript
-                </li>
-                <li className={utilStyles.smallListItem}>
-                  Node Fetch
-                </li>
+                <li className={utilStyles.smallListItem}>TypeScript</li>
+                <li className={utilStyles.smallListItem}>Node Fetch</li>
               </ul>
             </small>
           </li>
@@ -46,43 +44,34 @@ export default function Home() {
             <br />
             <small className={utilStyles.lightText}>
               <small className={utilStyles.bold}>Sept. 2019 - Present </small>
-              <br />
-              A discord community that unites web developers
+              <br />A discord community that unites web developers
             </small>
           </li>
 
           <li className={utilStyles.listItem}>
             <Link href="https://htcolab.org/">
-              <a target="_blank">Volunteer - Diabetes Connect-A-Thon</a>
+              <a target="_blank">Diabetes Connect-A-Thon - Volunteer</a>
             </Link>
             <br />
             <small className={utilStyles.lightText}>
               <small className={utilStyles.bold}>Mar. 2018 - Jun. 2018 </small>
               <br />
-              A volunteer software engineer working with modern websites and mobile applications. In partnership with the Digital Health Collaborative.
+              A volunteer software engineer working with modern websites and
+              mobile applications. In partnership with the Digital Health
+              Collaborative.
               <br />
-              Stack Used
+              Stack Used:
               <ul className={utilStyles.smallList}>
-                <li className={utilStyles.smallListItem}>
-                  TypeScript
-                </li>
-                <li className={utilStyles.smallListItem}>
-                  KoaJS
-                </li>
-                <li className={utilStyles.smallListItem}>
-                  MongoDB
-                </li>
-                <li className={utilStyles.smallListItem}>
-                  NativeScript
-                </li>
-                <li className={utilStyles.smallListItem}>
-                  Flutter
-                </li>
+                <li className={utilStyles.smallListItem}>TypeScript</li>
+                <li className={utilStyles.smallListItem}>KoaJS</li>
+                <li className={utilStyles.smallListItem}>MongoDB</li>
+                <li className={utilStyles.smallListItem}>NativeScript</li>
+                <li className={utilStyles.smallListItem}>Flutter</li>
               </ul>
             </small>
           </li>
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
